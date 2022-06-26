ff7622040014ac550c2412f441f09de03d71e7b0
Jude Martin <judemartin.mj@gmail.com>
2022-06-26 09:02:09 -0500

    Product preview completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,84 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
-const Home: NextPage = () => {
+import { NextPage } from "next";
+import Image from "next/image";
+import React from "react";
+import IconCart from "./components/IconCart";
+const ProductPreview: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <section className="bg-cream flex min-h-screen justify-center">
+      <div className="mx-4 mt-7 mb-5 max-w-[345px] xl:max-w-[600px] xl:hidden">
+        <div className="xl:hidden">
+          <Image
+            className="rounded-t-lg xl:hidden"
+            src="/images/product-preview/product-preview-mobile.jpg"
+            alt="Vercel Logo"
+            height={240}
+            width={343}
+          />
         </div>
-      </main>
+        <div className="bg-white rounded-b-lg pb-6 xl:hidden">
+          <div className="mx-6 -mt-2">
+            <h2 className="uppercase pt-4 font-montserrat font-normal leading-4 tracking-[5px] text-xs text-aurometal">
+              PERFUME
+            </h2>
+            <h2 className="mt-3 font-fraunces text-3xl font-bold text-gunmetal leading-8">
+              Gabrielle Essence Eau De Parfum
+            </h2>
+            <p className="mt-4 font-montserrat text-sm  leading-6 font-medium text-aurometal">
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </p>
+            <h2 className="text-deepaquamarine mt-6 font-fraunces font-bold text-[32px]">
+              $149.99
+              <span className="font-montserrat font-normal line-through text-aurometal ml-6 text-[13px] text-center inline-block align-middle">
+                $169.99
+              </span>
+            </h2>
+            <button className="bg-deepaquamarine h-12 w-72  mt-5 rounded-lg text-white text-[14px] font-montserrat font-bold tracking-wide flex items-center justify-center space-x-2 active:bg-deepaquamarinedark">
+              <div>
+                <IconCart />
+              </div>
+              <div>Add to Cart</div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="hidden xl:flex xl:mx-auto xl:max-h-[450px]  xl:mt-44 xl:bg-white rounded-lg mb-10">
+        <div>
+          <Image
+            className="rounded-l-lg xl:hidden"
+            src="/images/product-preview/product-preview-desktop.jpg"
+            alt="Vercel Logo"
+            height={450}
+            width={300}
+          />
+        </div>
+        <div className="xl:max-h-[450px] xl:max-w-[300px] mx-8">
+          <h2 className="uppercase mb-5 pt-[22px] font-montserrat font-normal leading-4 tracking-[5px] text-xs text-aurometal">
+            PERFUME
+          </h2>
+          <h2 className="mb-5 font-fraunces text-3xl font-bold text-gunmetal leading-8 h-24 w-60">
+            Gabrielle Essence Eau De Parfum
+          </h2>
+          <p className="mb-7 font-montserrat text-sm leading-6 font-medium text-aurometal h-24 w-60">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <h2 className="text-deepaquamarine mb-7 font-fraunces font-bold text-[32px]">
+            $149.99
+            <span className="font-montserrat font-normal line-through text-aurometal ml-6 text-[13px] text-center inline-block align-middle">
+              $169.99
+            </span>
+          </h2>
+          <button className="bg-deepaquamarine h-12 w-60 mb-7 rounded-lg text-white text-[14px] font-montserrat font-semibold tracking-wide flex items-center justify-center space-x-2 active:bg-deepaquamarinedark">
+            <div>
+              <IconCart />
+            </div>
+            <div>Add to Cart</div>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default ProductPreview;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: "#f2ebe3",
+        gunmetal: "#1C232B",
+        aurometal: "#6C7289",
+        deepaquamarine: "#3D8168",
+        deepaquamarinedark: "#1A4032",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        fraunces: ["Fraunces"],
+      },
+      screens: {
+        xl: "1440px",
+      },
+    },
   },
   plugins: [],
-}
+};
